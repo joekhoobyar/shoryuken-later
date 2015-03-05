2fd18242740badcfb4c6c18da4ff4bebd6bde6a4
Joe Khoobyar <joe@khoobyar.name>
2015-03-05 14:53:59 -0500

    schedule polling

--- lib/shoryuken/later/poller.rb
+++ lib/shoryuken/later/poller.rb
@@ -3,17 +3,57 @@ require 'json'
 module Shoryuken
   module Later
     class Poller
-      #include Celluloid
+      include Celluloid
       include Shoryuken::Util
+      
+      attr_reader :table_name
+      
+      def initialize(manager, table_name)
+        @manager = manager
+        @table_name = table_name
+      end
+      
+      def poll
+        watchdog('Later::Poller#poll died') do
+          started_at = Time.now
+          
+          logger.debug { "Polling for scheduled messages in '#{@table_name}'" }
+          
+          begin
+            while item = next_item
+              id = item.attributes['id']
+              logger.info "Found message #{id} from '#{@table_name}'"
+              defer do
+                if sent_msg = process_item(item)
+                  logger.debug { "Enqueued message #{id} from '#{@table_name}' as #{sent_msg.id}" }
+                else
+                  logger.debug { "Skipping already queued message #{id} from '#{@table_name}'" }
+                end
+              end
+            end
+  
+            logger.debug { "Poller for '#{@table_name}' completed in #{elapsed(started_at)} ms" }
+          rescue => ex
+            logger.error "Error fetching message: #{ex}"
+            logger.error ex.backtrace.first
+          end
+            
+          @manager.async.poller_done(@table_name, self)
+        end
+      end
 
     private
     
-      def next_item(table)
-        Shoryuken::Later::Client.tables(table).items.
-          where(:perform_at).less_than((Time.now + Shoryuken::Later::MAX_QUEUE_DELAY).to_i).
-          first
+      def table
+        Shoryuken::Later::Client.tables(@table_name)
+      end
+
+      # Fetches the next available item from the schedule table.    
+      def next_item
+        table.items.where(:perform_at).less_than((Time.now + Shoryuken::Later::MAX_QUEUE_DELAY).to_i).first
       end
     
+      # Processes an item and enqueues it (unless another actor has already enqueued it).
       def process_item(item)
         time, worker_class, args, id = item.attributes.values_at('perform_at','shoryuken_class','shoryuken_args','id')
         
