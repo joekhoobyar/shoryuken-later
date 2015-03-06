97a291aebd920da9042be80c39b0e5a106847d16
Joe Khoobyar <joe@khoobyar.name>
2015-03-06 13:52:26 -0500

    adding specs

--- shoryuken-later.gemspec
+++ shoryuken-later.gemspec
@@ -22,7 +22,7 @@ Gem::Specification.new do |spec|
   
   spec.required_ruby_version = '>= 1.9.3'
   
-  spec.add_development_dependency "bundler", '~> 1.6'
+  spec.add_development_dependency "bundler", '>= 1.3.5'
   spec.add_development_dependency "rake",    '~> 10.0'
   spec.add_development_dependency "rspec",   '~> 3.0', '< 3.1'
 
---
+++ spec/spec_helper.rb
@@ -0,0 +1,74 @@
+require 'bundler/setup'
+Bundler.setup
+
+require 'celluloid'
+require 'shoryuken-later'
+require 'json'
+
+options_file = File.join(File.expand_path('../..', __FILE__), 'shoryuken.yml')
+
+$options = {}
+
+if File.exists? options_file
+  $options = YAML.load(File.read(options_file)).deep_symbolize_keys
+
+  AWS.config $options[:aws]
+end
+
+Shoryuken.logger.level = Logger::UNKNOWN
+Celluloid.logger.level = Logger::UNKNOWN
+
+# For Ruby 1.9
+module Kernel
+  def Hash(arg)
+    case arg
+    when NilClass
+      {}
+    when Hash
+      arg
+    when Array
+      Hash[*arg]
+    else
+      raise TypeError
+    end
+  end unless method_defined? :Hash
+end
+
+# For Ruby 1.9
+class Hash
+  def to_h
+    self
+  end unless method_defined? :to_h
+end
+
+class TestWorker
+  include Shoryuken::Worker
+
+  shoryuken_options queue: 'shoryuken_later', schedule_table: 'shoryuken_later'
+
+  def perform(sqs_msg, body); end
+end
+
+RSpec.configure do |config|
+  config.before do
+    Shoryuken::Later::Client.class_variable_set :@@tables, {}
+
+    Shoryuken::Later.options.clear
+    Shoryuken::Later.options.merge!($options)
+
+    Shoryuken::Later.tables.replace(['shoryuken_later'])
+
+    Shoryuken::Later.options[:later][:delay] = 60
+    Shoryuken::Later.options[:later][:tables] = ['shoryuken_later']
+    Shoryuken::Later.options[:timeout]       = 1
+
+    Shoryuken::Later.options[:aws] = {}
+
+    TestWorker.get_shoryuken_options.clear
+    TestWorker.get_shoryuken_options['queue'] = 'shoryuken_later'
+    TestWorker.get_shoryuken_options['schedule_table'] = 'shoryuken_later'
+
+    Shoryuken.workers.clear
+    Shoryuken.register_worker('shoryuken_later', TestWorker)
+  end
+end
