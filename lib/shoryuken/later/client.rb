6d68991f129035cf9b6abb51dcf9de44826acfba
Chris Kalafarski <chris@farski.com>
2015-04-04 12:45:26 -0400

    Create/delete items based on hash/range keys

--- lib/shoryuken/later/client.rb
+++ lib/shoryuken/later/client.rb
@@ -20,15 +20,40 @@ module Shoryuken
         end
         
         def create_item(table, item)
-          item['id'] ||= SecureRandom.uuid
-          
-          ddb.put_item(table_name: table, item: item,
-                       expected: {id: {exists: false}})
+          # Items are intended for tables with hash+range primary keys. The
+          # `scheduler` hash key is essentially meaningless, it but a hash key
+          # is required. By having a meaningful range key that can also provide
+          # uniqueness, querying for pending items is fairly efficient.
+
+          item['scheduler'] = 'shoryuken-later'
+
+          # Add a random fraction to maintain unique range keys for jobs with
+          # a single second
+          # (DynamoDB take Number values as Strings to avoid precision issues)
+          item['perform_at'] = "#{item['perform_at']}.#{Random.rand(2 ** 64)}"
+
+          # Mostly for record keeping
+          item['id'] = SecureRandom.uuid
+
+          params = {
+            table_name: table,
+            item: table,
+            # condition_expression: "" # TODO `expected` is deprecated
+            # expected: { perform_at: { exists: false } }
+          }
         end
         
         def delete_item(table, item)
-          ddb.delete_item(table_name: table, key: {id: item['id']},
-                          expected: {id: {value: item['id'], exists: true}})
+          params = {
+            table_name: table,
+            key: {
+              scheduler: item['scheduler'],
+              perform_at: item['perform_at']
+            },
+            # expected: { perform_at: { value: item['perform_at'], exists: true } } # TODO Deprecated
+          }
+
+          ddb.delete_item(params)
         end
         
         def ddb
